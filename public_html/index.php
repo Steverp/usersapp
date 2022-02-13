52e1a108ddb2fd89ca231060b1c04153653534e2
Steve Romero <steverp95@outlook.es>
2022-02-12 19:39:00 -0500

    Added twig template engine to render data

--- public_html/index.php
+++ public_html/index.php
@@ -1,20 +1,15 @@
 <?php
-// Autoload files using composer
-require_once '../vendor/autoload.php';
-require_once '../src/Controller/UserManagement.php';
-require_once '../src/Controller/CustomerData.php';
 
-use \src\classes\UserManagement;
-use \src\classes\CustomerData;
+require_once '../bootstrap.php';
+/**
+ * @var $twig
+ */
+// Sample data
+$foo = [
+    [ 'name'          => 'Alice' ],
+    [ 'name'          => 'Bob' ],
+    [ 'name'          => 'Eve' ],
+];
 
-$valueToFilter= $_POST['valueToFilter'];
-
-$customer = new CustomerData();
-
-$userId = $customer->getOneCustomer($valueToFilter);
-var_dump($userId); die;
-
-//$userManagement = new UserManagement();
-//
-//var_dump($userManagement->updateUser());
-
+// Render our view
+echo $twig->render('index.html.twig', ['foo' => $foo] );
